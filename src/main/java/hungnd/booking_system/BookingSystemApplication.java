b8c58d102d33e66dc2d6cbcf6f2adbeaf9a5b6a6
hung.nd <hung.nd183547@sis.hust.edu.vn>
2021-08-01 20:24:16 +0700

    clear unused import

--- src/main/java/hungnd/booking_system/BookingSystemApplication.java
+++ src/main/java/hungnd/booking_system/BookingSystemApplication.java
@@ -1,16 +1,13 @@
 package hungnd.booking_system;
 
-import hungnd.booking_system.global.ConfigInfo;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Properties;
 
 @SpringBootApplication
 @Controller
@@ -22,13 +19,7 @@ public class BookingSystemApplication {
         return "Hello World!";
     }
     public static void main(String[] args) {
-        Properties prop = new Properties();
-        prop.put("server.port", ConfigInfo.SERVICE_PORT);
-
-        SpringApplicationBuilder applicationBuilder = new SpringApplicationBuilder()
-                .sources(BookingSystemApplication.class)
-                .properties(prop);
-        applicationBuilder.run(args);
+        SpringApplication.run(BookingSystemApplication.class, args);
     }
 
     @Bean
