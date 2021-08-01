7d5d4b188ad3589e6f2f9b14d10d9043c3a9328e
hung.nd <hung.nd183547@sis.hust.edu.vn>
2021-08-01 20:11:35 +0700

    add test

--- src/main/java/hungnd/booking_system/BookingSystemApplication.java
+++ src/main/java/hungnd/booking_system/BookingSystemApplication.java
@@ -4,14 +4,23 @@ import hungnd.booking_system.global.ConfigInfo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Properties;
 
 @SpringBootApplication
+@Controller
 public class BookingSystemApplication {
 
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
     public static void main(String[] args) {
         Properties prop = new Properties();
         prop.put("server.port", ConfigInfo.SERVICE_PORT);
